d712c0bfbe1519f1e69431f6f14641d9c42d0f16
dodar123 <115532375+dodar123@users.noreply.github.com>
2022-12-01 10:30:39 +0100

    Add files via upload

---
+++ aktuelles.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Aktuelles</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+
+        <link href="css/styles.css" rel="stylesheet" />
+        <link href="css/style.css" rel="stylesheet" />
+        <link href="css/carousel.css" rel="stylesheet">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400&family=Quicksand:wght@500&family=Rajdhani:wght@300&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&family=Quicksand:wght@500&family=Rajdhani:wght@300&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <!-- Responsive navbar-->
+        <header class="header">
+          <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+            <div class="container">
+              <a
+                class="navbar-brand .align-top .align-middle .align-bottom"
+                href="index.php"
+                ><img class="logo" src="img/Logo/logo-main - Kopie.png"
+              /></a>
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="dienstleistungen.php"
+                      >Dienstleistung</a
+                    >
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="laborausstattung.php"
+                      >Laborausstattung</a
+                    >
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" href="aktuelles.php"
+                      >Aktuelles und Veranstaltungen</a
+                    >
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="archiv.php">Archiv/Galerie</a>
+                  </li>
+
+		 <li class="nav-item">
+                    <a class="nav-link" href="Lebenslauf.php">Kontakt</a>
+                  </li>
+               </ul>
+              </div>
+            </div>
+          </nav>
+        </header>
+    <!-- Ende Responsive navbar-->
+
+        <!--Main-->
+        <main id="main">
+
+          <!-- ======= Breadcrumbs ======= -->
+          <section id="breadcrumbs" class="breadcrumbs">
+            <div class="container" style="margin-top: 4rem;">
+              <h2>Aktuelles / Veranstaltungen</h2>
+              
+            </div>
+
+
+          <!-- ======= Inhalt Aktuelles / Veranstaltungen ======= -->
+          
+
+            <!-- Erster Punkt -->
+            <section id="inner-page" class="dienstleistungen">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-5 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0"> 
+                  <div class="icon-box">
+                      <h4 class="title"><a href="Labor4.php">Ausstattung  des  Labors  für  optische  Messtechnik  mit  einem  3D  Streifenprojektions-Sensor</a></h4> <!-- Link zur Labor4 Unterseite -->
+                      <p class="description"><a href="Labor4.php">Hier gelangen Sie zum Scan Cobot GOM ATOS Q</a></p>
+                  </div>
+                </div>
+                
+                <div class="col-md-5 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0"> 
+                  <div class="icon-box">
+                      <h4 class="title"><a href="aktuelles_u1.php">D- (Design-) FMEA-Training am 11. und 12. Januar 2022</a></h4>
+                      <p class="description"> <li>Ziele</li>
+                                              <li>Ablauf</li>
+                                              <li>Anmeldung</li></p>
+                  </div>
+                </div>
+                <!-- Ende Erster Punkt -->
+               
+                <!-- Zweiter Punkt -->
+                <div class="col-md-5 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0"> 
+                  <div class="icon-box">
+                      <h4 class="title"><a href="aktuelles_u2.php">Neues BMBF-gefördertes Forschungsprojekt „AdaptSave“ zum Thema Feuerlöscher</a></h4>
+                      <p class="description">Feuerlöscher und Löschanlagen sind unumgängliche und notwendige Einrichtungen für einen sicheren
+                  Personentransport. Aber nicht jedes Transportmittel verfügt über den notwendigen Stauraum...</p>
+                  </div>
+                </div>
+                <!-- Ende Zweiter Punkt -->
+
+                <!-- Dritter Punkt -->
+                <div class="col-md-5 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0"> 
+                  <div class="icon-box">
+                      <h4 class="title"><a href="https://www.utoronto.ca/" target="_blank">Gastprofessur an der University of Toronto </a></h4> <!-- Link zur Universität Toronto -->
+                      <p class="description">Seit Januar 2022 ist Prof. Dr.-Ing. Nico Blessing zusätzlich Gastprofessor an der University of Toronto.</br><a href="https://www.utoronto.ca/" target="_blank"> Hier gelangen Sie zur Website der University of Toronto</a></p>
+                  </div>
+                </div>
+                <!-- Ende Dritter Punkt -->
+
+                <!-- Vierter Punkt -->
+                <div class="col-md-5 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" style="margin-top: 30px;"> 
+                  <div class="icon-box">
+                      <h4 class="title"><a href="aktuelles_u3.php">Anlieferung der neuen Materialprüfmaschine im Labor für Werkstoffprüfung und -charakterisierung</a></h4>
+                      <p class="description">Die neue Materialprüfmaschine wurde im März geliefert und wird in kürze in Betrieb genommen.</p>
+                  </div>
+                </div>
+                <!-- Ende Vierter Punkt -->
+
+                <!-- Fünfter Punkt -->
+                <div class="col-md-5 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" style="margin-top: 30px;"> 
+                  <div class="icon-box">
+                      <h4 class="title"><a href="aktuelles_u4.php">Besuch der neuen Präsidentin der DHBW</a></h4>
+                      <p class="description">Ende März 2022 war die neue Präsidentin der DHBW, Frau Prof. Martina Klärle, am Standort Heidenheim zu Besuch und hat unter anderem die Labore besichtigt.</p>
+                  </div>
+                </div>
+                <!-- Ende Fünfter Punkt -->
+
+                <!-- Sechster Punkt -->
+                <div class="col-md-5 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" style="margin-top: 30px;"> 
+                  <div class="icon-box">
+                      <h4 class="title"><a href="files/RunderGeburtstag.pdf" target="_blank">10 jähriges Bestehen des Transferzentrums</a></h4> <!-- pdf Datei vom runden Geburtstag -->
+                      <p class="description"><a href="files/RunderGeburtstag.pdf">Schreiben zum runden Geburtstag</a></p>
+                  </div>
+                </div>
+                <!-- Ende Sechster Punkt -->
+
+              
+          </section> <!-- Ende Inhalt Aktuelles / Veranstaltungen -->
+
+        </main><!-- Ende #main -->
+
+        <!--Footer-->
+    <div class="footer-container">
+      <footer class="py-3 my-4">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+          <li class="nav-item">
+            <a href="index.php" class="nav-link px-2 text-white">Home</a>
+          </li>
+          <li class="nav-item">
+            <a href="Lebenslauf.php" class="nav-link px-2 text-white">Kontakt </a>
+          </li>
+          <li class="nav-item">
+            <a href="impressum.php" class="nav-link px-2 text-white">Impressum</a>
+          </li>
+          <li class="nav-item">
+            <a href="datenschutz.php" class="nav-link px-2 text-white">Datenschutz</a>
+          </li>
+        </ul>
+        <p class="text-center text-white">Steinbeis</p>
+      </footer>
+  </div>
+  <!--Ende Footer-->
+
+  <!-- Bootstrap core JS-->
+  <script src="js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+
+ 
+  </body>
+</html>
